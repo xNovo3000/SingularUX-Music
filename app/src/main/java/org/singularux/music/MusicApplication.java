6be811116d17dfce71697e2f9273d5a698da4b5c
xNovo3000 <fontana.ciro.2001@gmail.com>
2026-04-03 16:12:34 +0200

    Added dependencies, basic theming and started with first application component

---
+++ app/src/main/java/org/singularux/music/MusicApplication.java
@@ -0,0 +1,9 @@
+package org.singularux.music;
+
+import android.app.Application;
+
+import dagger.hilt.android.HiltAndroidApp;
+
+@HiltAndroidApp
+public class MusicApplication extends Application {
+}
