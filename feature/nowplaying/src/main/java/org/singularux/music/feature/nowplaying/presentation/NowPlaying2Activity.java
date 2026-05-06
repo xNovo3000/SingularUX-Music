a6dcfc449636c455021d61a3fc5c67e4ce2b2796
xNovo3000 <fontana.ciro.2001@gmail.com>
2026-05-06 13:17:25 +0200

    Added basic activity and viewmodel

--- feature/nowplaying/src/main/java/org/singularux/music/feature/nowplaying/presentation/NowPlaying2Activity.java
+++ feature/nowplaying/src/main/java/org/singularux/music/feature/nowplaying/presentation/NowPlaying2Activity.java
@@ -1,6 +1,43 @@
 package org.singularux.music.feature.nowplaying.presentation;
 
-import androidx.activity.ComponentActivity;
+import android.os.Bundle;
 
+import androidx.activity.ComponentActivity;
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.Nullable;
+import androidx.lifecycle.ViewModelProvider;
+
+import org.singularux.music.core.playback.MusicControllerFacade;
+import org.singularux.music.feature.nowplaying.databinding.ActivityNowPlaying2Binding;
+
+import javax.inject.Inject;
+
+import dagger.hilt.android.AndroidEntryPoint;
+
+@AndroidEntryPoint
 public class NowPlaying2Activity extends ComponentActivity {
+
+    private @Inject MusicControllerFacade musicControllerFacade;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        // Create activity with edge-to-edge support
+        EdgeToEdge.enable(this);
+        super.onCreate(savedInstanceState);
+        // Extract XML elements
+        ActivityNowPlaying2Binding binding = ActivityNowPlaying2Binding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        // TODO: Add inset listeners
+        // Extract ViewModel data
+        NowPlaying2ViewModel viewModel = new ViewModelProvider(this)
+                .get(NowPlaying2ViewModel.class);
+    }
+
+    @Override
+    protected void onDestroy() {
+        // Release controller before finishing this activity
+        if (isFinishing()) musicControllerFacade.release();
+        super.onDestroy();
+    }
+
 }
---
+++ feature/nowplaying/src/main/java/org/singularux/music/feature/nowplaying/presentation/NowPlaying2ViewModel.java
@@ -0,0 +1,9 @@
+package org.singularux.music.feature.nowplaying.presentation;
+
+import androidx.lifecycle.ViewModel;
+
+import dagger.hilt.android.lifecycle.HiltViewModel;
+
+@HiltViewModel
+public class NowPlaying2ViewModel extends ViewModel {
+}
