d30bd0b634a91bf176151a56d484ea47d6f88ac6
xNovo3000 <fontana.ciro.2001@gmail.com>
2026-05-16 16:04:54 +0200

    Removed :feature:tracklist and more light build.gradle files

--- feature/tracklist/src/main/java/org/singularux/music/feature/tracklist/ui/search/SearchListOnClickListener.java
+++
@@ -1,7 +0,0 @@
-package org.singularux.music.feature.tracklist.ui.search;
-
-import org.singularux.music.feature.tracklist.ui.search.item.SearchListItemTrack;
-
-public interface SearchListOnClickListener {
-    void onClick(SearchListItemTrack itemTrack);
-}
